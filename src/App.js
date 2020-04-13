0b2002876efb2a06fa2f74e82a42fe4b799eed25
Shiva <sivaraj.v@refinitiv.com>
2020-04-13 13:01:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Tabletop from 'tabletop';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    Tabletop.init({
+      key: '1TVeWU1bV8tI4-DQ1XIBrwPeERMKEDUm5Kjao2rPdNiU',
+      callback: googleData => {
+        console.log('google sheet data --->', googleData)
+        this.setState({
+          data: googleData
+        })
+      },
+      simpleSheet: true
+    })
+  }
+
+  render() {
+    const { data } = this.state
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">React + Google Sheets Demo</h1>
+        </header>
+        <div id="employee-details">
+          {
+            data.map(obj => {
+              return (
+                <div key={obj.employee}>
+                  <p>{obj.employee}</p>
+                  <p>{obj.favDog}</p>
+                  <img alt={obj.favDog} src={obj.img} />
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
